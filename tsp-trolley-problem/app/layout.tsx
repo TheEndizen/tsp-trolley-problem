e9482efa4e5a277c38b8c9bb9f8c718835eb4095
Colin Sun <nilocnus@Colins-MacBook-Air-2.local>
2025-01-18 18:35:45 -0800

    funny typing effect

--- tsp-trolley-problem/app/layout.tsx
+++ tsp-trolley-problem/app/layout.tsx
@@ -1,12 +1,5 @@
 import type { Metadata } from "next";
-import { Comic_Neue, Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const comicNeue = Comic_Neue({
-  variable: "--font-comic-neue",
-  subsets: ["latin"],
-  weight: "300"
-})
 
 export const metadata: Metadata = {
   title: "Travelling Salesman Trolley Problem",
@@ -21,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`font-patrickHand bg-white antialiased`}
+        className={`font-patrickHand bg-white`}
       >
         {children}
       </body>
